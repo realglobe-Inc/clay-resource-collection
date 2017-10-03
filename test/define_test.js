8878879e95d7869bf5bd5192d18d1a88f296d691
okunishinishi <okunishinishi@gmail.com>
2017-10-03 11:38:49 +0900

    use async/await

--- test/define_test.js
+++ test/define_test.js
@@ -5,24 +5,22 @@
 'use strict'
 
 const define = require('../lib/define.js')
-const { fromDriver } = require('clay-resource')
+const {fromDriver} = require('clay-resource')
 const clayDriverMemory = require('clay-driver-memory')
-const { equal, ok } = require('assert')
-
-const co = require('co')
+const {equal, ok} = require('assert')
 
 describe('define', function () {
   this.timeout(3000)
 
-  before(() => co(function * () {
+  before(async () => {
 
-  }))
+  })
 
-  after(() => co(function * () {
+  after(async () => {
 
-  }))
+  })
 
-  it('Define', () => co(function * () {
+  it('Define', async () => {
     let driver = clayDriverMemory({})
     let Org = fromDriver(driver, 'Org')
     let User = fromDriver(driver, 'User')
@@ -38,34 +36,34 @@ describe('define', function () {
     for (let i = 0; i < 100; i++) {
       if (i < 50) {
         orgs.push(
-          yield Org.create({ name: `org-${i}` })
+          await Org.create({name: `org-${i}`})
         )
       }
       users.push(
-        yield User.create({ name: `org-${i}`, org: orgs[ parseInt(i % 10) ] })
+        await User.create({name: `org-${i}`, org: orgs[parseInt(i % 10)]})
       )
     }
 
     {
 
       let userCollection01 = new UserCollection(
-        yield User.list({ filter: { org: orgs[ 1 ] }, page: { size: 5, number: 1 } })
+        await User.list({filter: {org: orgs[1]}, page: {size: 5, number: 1}})
       )
       ok(userCollection01.hasNext)
       ok(!userCollection01.hasPrev)
 
-      let userCollection02 = yield userCollection01.next()
+      let userCollection02 = await userCollection01.next()
 
       ok(userCollection02.hasPrev)
 
-      let userCollection03 = yield userCollection02.prev()
+      let userCollection03 = await userCollection02.prev()
       ok(!userCollection03.hasPrev)
 
-      let userCollection04 = yield userCollection02.next()
+      let userCollection04 = await userCollection02.next()
       ok(userCollection04.hasPrev)
       ok(!userCollection04.hasNext)
     }
-  }))
+  })
 })
 
 /* global describe, before, after, it */
